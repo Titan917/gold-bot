b17422ffb11aa899ed39ca3ba1189c584f245c4e
Titan917 <tgvantage886@gmail.com>
2026-05-22 07:23:36 +0530

    Update main.py with Telegram bot integration code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+import time
+import requests
+
+TELEGRAM_TOKEN = "8867215330:AAEbnbwqsU_090PChnT8iqFqyZCr-oN_UvU"
+CHAT_ID = "6511464561"
+
+def send_telegram_msg(message):
+    url = f"https://api.telegram.com/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": message, "parse_mode": "Markdown"}
+    try:
+        requests.post(url, json=payload)
+    except Exception as e:
+        print("Error:", e)
+
+print("🚀 SERVER STARTED SUCCESSFULLY...")
+send_telegram_msg("🔄 *Gyananshu Bhai! Gold Bot Render Server Par Live Ho Chuka Hai!*\nTeeno Models background me active hain.")
